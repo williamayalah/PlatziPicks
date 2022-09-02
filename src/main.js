5e9bd794d5ab865ecd414e1da83e7824782bbfcd
William Ayala <william.ayala.huacon@gmail.com>
2022-09-01 21:22:25 -0500

    [ADD] IPC for channel:openDirectory

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 "use strict";
 
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const devTools = require("./devtools");
 const url = require("url");
 const path = require("path");
+
+let win;
 
 if (process.env.NODE_ENV === "development") {
   devTools();
 }
 
 function createWindow() {
-  let win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 1200,
     height: 800,
     maximizable: false,
@@ -33,10 +35,6 @@ function createWindow() {
   });
 }
 
-function getFileName(event, fileSrc) {
-  return path.basename(fileSrc);
-}
-
 app.whenReady().then(() => {
   createWindow();
 
@@ -45,8 +43,24 @@ app.whenReady().then(() => {
       createWindow();
     }
   });
-  
-  ipcMain.handle("image:fileName", getFileName);
+
+  //IPC
+  ipcMain.handle("channel:getFileName", (event, fileSrc) => {
+    return path.basename(fileSrc);
+  });
+
+  ipcMain.on("channel:openDirectory", (event) => {
+    dialog.showOpenDialog(win, {
+        title: "Seleccione la ubicación",
+        buttonLabel: "Abrir ubicación",
+        properties: ["openDirectory"],
+    }).then((result) => {
+        console.log(result.canceled);
+        console.log(result.filePaths);
+    }).catch((err) => {
+        console.log(err)
+      });
+  });
 });
 
 app.on("window-all-closed", () => {
